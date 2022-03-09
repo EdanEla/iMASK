1a9cd295b79f9c3724fa47aef6075b016e1da63f
samcho02 <sc107@illinois.edu>
2022-03-08 18:51:01 -0600

    line detection

--- hi.py
+++ hi.py
@@ -1,1 +1,13 @@
-print("hi")
+import cv2 as cv
+import numpy as np 
+
+road = cv.imread("Photos/road1.jpg")
+
+# takes canny edges of the road 
+def Canny(road):
+    # Gaussian blur for the noise
+    blur_road = cv.GaussianBlur(road, (3,3), cv.BORDER_DEFAULT) 
+    canny_road = cv.Canny(blur_road, 125, 175)
+
+    return canny_road
+
